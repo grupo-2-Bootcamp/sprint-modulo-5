40e83a83769a42377ab82d58c2d9b34da4011737
Eduardo Escárez <eduardoe@gmail.com>
2023-05-29 19:07:20 -0400

    Completa las funciones y ordena un poco

--- sprint.sql
+++ sprint.sql
@@ -4,111 +4,84 @@
 -- EDUARDO ESCAREZ GOMEZ
 
 
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
-  `id_usuario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(20) NOT NULL,
-  `apellido` VARCHAR(20) NOT NULL,
-  `edad` INT NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `cantidad_usos` INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  UNIQUE INDEX `id_usuario_UNIQUE` (`id_usuario` ASC) VISIBLE)
-ENGINE = InnoDB;
+-- Crea el schema y lo utiliza
+CREATE SCHEMA telovento_pedidos;
+USE telovento_pedidos;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`operario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`operario` (
-  `id_operario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `edad` INT NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `veces_soporte` INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id_operario`),
-  UNIQUE INDEX `id_operario_UNIQUE` (`id_operario` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
+-- Crea tabla usuario
+CREATE TABLE usuario (
+    id_usuario INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    apellido VARCHAR(20) NOT NULL,
+    edad INT NOT NULL,
+    correo_electronico VARCHAR(45) NOT NULL,
+    cantidad_usos INT NOT NULL DEFAULT 1,
+    UNIQUE INDEX correo_electronico_UNIQUE (correo_electronico ASC) VISIBLE,
+    UNIQUE INDEX id_usuario_UNIQUE (id_usuario ASC) VISIBLE);
 
 
--- -----------------------------------------------------
--- Table `mydb`.`soporte`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`soporte` (
-  `id_soporte` INT NOT NULL AUTO_INCREMENT,
-  `id_usuario` INT UNSIGNED NOT NULL,
-  `id_operario` INT UNSIGNED NOT NULL,
-  `fecha` DATE NOT NULL,
-  `evaluacion` TINYINT(1) NOT NULL,
-  `comentario` TINYTEXT NULL,
-  PRIMARY KEY (`id_soporte`),
-  INDEX `fk_soporte_usuario_idx` (`id_usuario` ASC) VISIBLE,
-  INDEX `fk_soporte_operario1_idx` (`id_operario` ASC) VISIBLE,
-  CONSTRAINT `fk_soporte_usuario`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `mydb`.`usuario` (`id_usuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_soporte_operario1`
-    FOREIGN KEY (`id_operario`)
-    REFERENCES `mydb`.`operario` (`id_operario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- Crea tabla operario
+CREATE TABLE operario (
+    id_operario INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(45) NOT NULL,
+    apellido VARCHAR(45) NOT NULL,
+    edad INT NOT NULL,
+    correo_electronico VARCHAR(45) NOT NULL,
+    veces_soporte INT NOT NULL DEFAULT 1,
+    UNIQUE INDEX id_operario_UNIQUE (id_operario ASC) VISIBLE,
+    UNIQUE INDEX correo_electronico_UNIQUE (correo_electronico ASC) VISIBLE);
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Crea tabla soporte
+CREATE TABLE soporte (
+    id_soporte INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT UNSIGNED NOT NULL,
+    id_operario INT UNSIGNED NOT NULL,
+    fecha DATE NOT NULL,
+    evaluacion TINYINT(1) NOT NULL,
+    comentario TINYTEXT NULL,
+    INDEX fk_soporte_usuario_idx (id_usuario ASC) VISIBLE,
+    INDEX fk_soporte_operario1_idx (id_operario ASC) VISIBLE,
+    CONSTRAINT fk_soporte_usuario
+        FOREIGN KEY (id_usuario)
+        REFERENCES usuario (id_usuario)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_soporte_operario1
+        FOREIGN KEY (id_operario)
+        REFERENCES operario (id_operario)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION);
 
 
+-- Crea usuario y le asigna permisos para acceder a la base de datos
 CREATE USER 'sprintBoss'@'localhost' IDENTIFIED BY 'awake*2023';
-GRANT ALL PRIVILEGES ON mydb.* TO 'sprintBoss'@'localhost';
+GRANT ALL PRIVILEGES ON telovento_pedidos.* TO 'sprintBoss'@'localhost';
 FLUSH PRIVILEGES;
 
--- Se seleciona por defecto la base de datos a trabajar
-USE mydb;
+
+-- Carga de datos a la tabla USUARIO
+INSERT INTO usuario (nombre, apellido, edad, correo_electronico, cantidad_usos)
+VALUES
+        ('Ximena', 'Rossa', 32, 'xrossa@gmail.com', 1),
+        ('Italo', 'Soto', 25, 'isoto@gmail.com', 2),
+        ('Marcelo', 'Tapia', 50, 'mtapia@gmail.com', 5),
+        ('Francisca', 'Aravena', 35, 'faravena@hotmail.com', 1),
+        ('Juan', 'Perez', 40, 'jperez@example.com', 1);
 
 
--- Carga de datos a la tabla USUARIOS
-INSERT INTO usuario (nombre, apellido, edad, email, cantidad_usos)
-VALUES ('Ximena', 'Rossa', 32, 'xrossa@gmail.com', 1),
-       ('Italo', 'Soto', 25, 'isoto@gmail.com', 2),
-       ('Marcelo', 'Tapia', 50, 'mtapia@gmail.com', 5),
-       ('Francisca', 'Aravena', 35, 'faravena@hotmail.com', 1),
-       ('Juan', 'Perez', 40, 'jperez@example.com', 1);
-
--- Carga de datos a la tabla OPERARIOS
-INSERT INTO operario (nombre, apellido, edad, email, veces_soporte)
-VALUES ('Juan', 'Pérez', 30, 'juan@example.com', 3),
-        ('María', 'Gómez', 25, 'maria@example.com', 5)
-        ('Carlos', 'López', 35, 'carlos@example.com', 10)
-        ('Ana', 'Sánchez', 28, 'ana@example.com', 2)
-        ('Luis', 'Hernández', 32, 'luis@example.com', 7);
+-- Carga de datos a la tabla OPERARIO
+INSERT INTO operario (nombre, apellido, edad, correo_electronico, veces_soporte)
+VALUES
+        ('Juan', 'Pérez', 30, 'juan@example.com', 3),
+        ('María', 'Gómez', 25, 'maria@example.com', 5),
+        ('Carlos', 'López', 35, 'carlos@example.com', 10),
+        ('Ana', 'Sánchez', 18, 'ana@example.com', 2),
+        ('Luis', 'Hernández', 19, 'luis@example.com', 7);
 
 
--- Carga de datos a la tabla soporte
+-- Carga de datos a la tabla SOPORTE
 INSERT INTO soporte (id_usuario, id_operario, fecha, evaluacion, comentario)
 VALUES
         (1, 2, '2023-05-22', 5, 'Buen servicio, resolvieron mi problema rápidamente.'),
@@ -124,38 +97,28 @@ VALUES
 
 
 -- Seleccione las 3 operaciones con mejor evaluacion.
-
-
-
+SELECT * FROM soporte ORDER BY evaluacion DESC LIMIT 3;
 
 
 -- Seleccione las 3 operaciones con menos evaluacion.
-
-
+SELECT * FROM soporte ORDER BY evaluacion ASC LIMIT 3;
 
 
 -- Seleccione al operario que mas soportes ha realizado.
-
-
-
-
+SELECT * FROM operario ORDER BY veces_soporte DESC LIMIT 1;
 
 -- Seleccione al cliente que menos veces ha utilizado la aplicacion.
+SELECT * FROM usuario ORDER BY cantidad_usos ASC LIMIT 1;
 
 
-
-
--- Agregue 10 anos a los tres primeros usuarios registrados.
-
-
+-- Agregue 10 años a los tres primeros usuarios registrados.
+UPDATE usuario SET edad = edad + 10 WHERE id_usuario < 4;
 
 
 -- Renombre todas las columnas correo electronico. El nuevo nombre debe ser email.
+ALTER TABLE usuario CHANGE correo_electronico email VARCHAR(45);
+ALTER TABLE operario CHANGE correo_electronico email VARCHAR(45);
 
 
-
-
--- Seleccione solo los operarios mayores de 20 anos.
-
-
-
+-- Seleccione solo los operarios mayores de 20 años.
+SELECT * FROM operario WHERE edad > 20;
